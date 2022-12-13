26326da91f2070d988a9d1b95541197836c5592d
Ivan Sysoev <ivansysoev007@gmail.com>
2022-12-14 00:14:02 +0300

    OGE_skamer_app

--- OGE_skamer.py
+++ OGE_skamer.py
@@ -4,7 +4,7 @@ from fake_useragent import UserAgent
 
 def first_try(id, first_exersize, last_exersize):
     """Функция принимает 3 аргумента (ID варианта ОГЭ, номер первого задания, номер последнего задания) и возращает ответы"""
-    answers = []
+    answers = {}
     url = f"https://rus-oge.sdamgia.ru/test?id={id}"
     response = requests.get(url, headers={"User-Agent": UserAgent().chrome})
     soup = BeautifulSoup(response.text, "lxml")
@@ -15,11 +15,21 @@ def first_try(id, first_exersize, last_exersize):
             exersize_id.append(b)
         except:
             None
+    k = first_exersize
     for ex_id in exersize_id[first_exersize - 1:last_exersize]:
         ex_url = f"https://rus-oge.sdamgia.ru/problem?id={ex_id}"
         response = requests.get(ex_url, headers={"User-Agent": UserAgent().chrome})
         soup = BeautifulSoup(response.text, "lxml")
         result = soup.find_all("p")[-1].text
-        answers.append(result)
+        answers[f"Задание №{k}"] = result[7 :-1]
+        k = k + 1
     return answers
-print(first_try(10025650, 2, 5))
+
+result = first_try(input(), int(input()), int(input()))
+for elem in result:
+    print(f"{elem}  =====> {result[elem]}")
+
+print("\n" + "-" * 41)
+print(" " * 10 + "press enter to exit")
+print("-" * 41)
+a = input()
